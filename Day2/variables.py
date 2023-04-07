ffac050596d56889b8c222914cef265217811327
Raphael Hessel <53626860+raphahessel@users.noreply.github.com>
2023-04-06 23:14:02 -0300

    Add files via upload

---
+++ Day2/variables.py
@@ -0,0 +1,36 @@
+# Variables in Python
+first_name = 'Raphael'
+last_name = 'Hessel'
+country = 'Brasil'
+city = 'Sao Paulo'
+age = 23
+is_married = False
+skills = ['pythin', 'sql', 'sas']
+person_info = {
+   'firstname':'Raphael',
+   'lastname':'Hessel',
+   'country':'Brasil',
+   'city':'Sao Paulo'
+   }
+
+print('hello,world')
+print('Hello',',','Hello Word')
+
+print('First name:', first_name)
+print('First name length:', len(first_name))
+print('Last name: ', last_name)
+print('Last name length: ', len(last_name))
+print('Country: ', country)
+print('City: ', city)
+print('Age: ', age)
+print('Married: ', is_married)
+print('Skills: ', skills)
+print('Person information: ', person_info)
+
+firt_name= input('What is your name?')
+print(first_name)
+
+firt_name_list= list(first_name)
+print(firt_name_list)
+
+#Complex Numbers= 1+j,2+4j, -1j
